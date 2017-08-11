47bf5e8819f587e27e14cf2ab512009fa0c684f6
夏冬_在线 <xiadong@cmos.chinamobile.com>
2017-08-11 18:02:30 +0800

    first version

---
+++ zeus-core/src/main/java/com/brianxia/zeus/App.java
@@ -0,0 +1,13 @@
+package com.brianxia.zeus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zeus-monitor/src/main/java/com/brianxia/zeus/zeusmonitor/ZeusMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.brianxia.zeus.zeusmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeusMonitorApplication.class, args);
+	}
+}
